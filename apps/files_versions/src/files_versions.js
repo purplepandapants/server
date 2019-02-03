e8d3710c1958f484a2ec54485f5d41058a2326c7
Roeland Jago Douma <roeland@famdouma.nl>
2019-02-03 19:56:44 +0100

    Initial move of files_versions to webpack
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

---
+++ apps/files_versions/src/files_versions.js
@@ -0,0 +1,16 @@
+// CSP config for webpack dynamic chunk loading
+// eslint-disable-next-line
+__webpack_nonce__ = btoa(OC.requestToken)
+
+// Correct the root of the app for chunk loading
+// OC.linkTo matches the apps folders
+// eslint-disable-next-line
+__webpack_public_path__ = OC.linkTo('files_versions', 'js/dist/')
+
+import './versionmodel'
+import './templates'
+import './versioncollection'
+import './versionstabview'
+import './filesplugin'
+
+window.OCA.Versions = OCA.Versions
